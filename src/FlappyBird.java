76845cfa425380745d757374da3cf6bd48f68f87
Astemir <s89009@stud.htw-dresden.de>
2025-04-06 23:25:18 +0200

    Added Bird

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -5,20 +5,31 @@ import java.util.ArrayList;
 import java.util.Random;
 
 
-
 public class FlappyBird extends JPanel {
+
+    //Images
     private Image birdImg;
     private Image backgroundImg;
     private Image topPipeImg;
     private Image bottomPipeImg;
+
+    //Panel Size
     private int width = 360;
     private int height = 640;
+
+    //Bird Position
+    private int birdX = width / 8;
+    private int birdY = height / 2;
+
+    Bird bird;
+
 
     public FlappyBird() {
         setPreferredSize(new Dimension(width, height));
 
         loadImages();
 
+        bird = new Bird(birdX, birdY, birdImg);
     }
 
     //load images
@@ -34,8 +45,34 @@ public class FlappyBird extends JPanel {
         super.paintComponent(g);
         draw(g);
     }
+
     private void draw(Graphics g) {
         g.drawImage(backgroundImg, 0, 0, width, height, null);
+        g.drawImage(birdImg, birdX, birdY, bird.getBirdWidth(), bird.getBirdHeight(), null);
     }
 
+    class Bird extends JPanel {
+        private int birdX;
+        private int birdY;
+
+        public int getBirdHeight() {
+            return birdHeight;
+        }
+
+        public int getBirdWidth() {
+            return birdWidth;
+        }
+
+        private int birdHeight;
+        private int birdWidth;
+        private Image image;
+
+        public Bird(int birdX, int birdY, Image image) {
+            this.image = image;
+            this.birdX = birdX;
+            this.birdY = birdY;
+            birdHeight = 24;
+            birdWidth = 34;
+        }
+    }
 }
